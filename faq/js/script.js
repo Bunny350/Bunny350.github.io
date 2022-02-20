1ed63f147bce460c072c178c623024f708aed5a9
William Pang <fry56774@gmail.com>
2022-02-20 13:20:16 +0800

    Added FAQ page and minor changes

---
+++ faq/js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-rigs","displayName":"Rigs","link":{"linkType":"LinkTypePage","href":"#!page-rigs"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
