4ff2142e91160576e80096c7e9c369aee2785402
William Pang <fry56774@gmail.com>
2022-02-20 13:32:38 +0800

    FAQ name fix

--- faq/js/script.js
+++ faq/js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-rigs","displayName":"Rigs","link":{"linkType":"LinkTypePage","href":"#!page-rigs"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-questions","displayName":"Questions","link":{"linkType":"LinkTypePage","href":"#!page-questions"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
