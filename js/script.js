ebee1d687ad7f6fa0d7de4d79e3647710986a001
William Pang <fry56774@gmail.com>
2022-02-22 22:16:08 +0800

    Available in limited time

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":550},"pages":[{"id":"page--","displayName":" ","link":{"linkType":"LinkTypePage","href":"#!page--"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page--","displayName":" ","link":{"linkType":"LinkTypePage","href":"#!page--"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
