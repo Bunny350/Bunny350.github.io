8742d3d9f9e23531971010ddd2da8574b1cb7406
William Pang <fry56774@gmail.com>
2022-02-23 22:28:51 +0800

    Restore from previous version and releasing wallpaper
    Since we hit the time limit of 22:22, we also added a new page that is dedicated for when we hit that time, users can view and download the images for free. Please visit this website for more information.

---
+++ feb22wallpaper/js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-questions","displayName":"Questions","link":{"linkType":"LinkTypePage","href":"#!page-questions"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
