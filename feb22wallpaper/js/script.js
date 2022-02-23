b9337089c9695044698c1159c4eb9bbbea7c7fc7
William Pang <fry56774@gmail.com>
2022-02-23 22:49:21 +0800

    Critical fix to LIMITED TAKE wallpapers page

--- feb22wallpaper/js/script.js
+++ feb22wallpaper/js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-questions","displayName":"Questions","link":{"linkType":"LinkTypePage","href":"#!page-questions"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"pagelimitedtake","displayName":"LIMITED TAKE Wallpapers","link":{"linkType":"LinkTypePage","href":"#!pagelimitedtake"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
