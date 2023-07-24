06ca434435efb9241361fa9d4de35311c74cd9e7
William Pang <fry56774@gmail.com>
2023-07-25 01:45:44 +0800

    Update 3
    Hugely revamped and issue fixes on mobile layout

---
+++ js/past-times.js
@@ -0,0 +1,3 @@
+let past_dates_list = '{"event_dates":[{"event_name":"cherry-grove-p1", "start_date":"2023-07-21T12:20:00Z", "end_date":"2023-07-21T14:50Z"}, {"event_name":"cherry-grove-p2", "start_date":"2023-07-22T12:00Z", "end_date":"2023-07-22T14:20Z"}, {"event_name":"cherry-grove-p3", "start_date":"2023-07-23T13:00Z", "end_date":"2023-07-23T15:00Z" }]}';
+// {"event_name":"event_name", "event_date":"YYYY-MM-DD", "otherdesc_begin":"To be announced", "otherdesc_after":"" }
+// This file shows past event dates.
