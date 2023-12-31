7c3ac9e6a64194893792755ae19fdc7f4d0172d7
Oitswilliam Pang <13944389+Bunny350@users.noreply.github.com>
2024-01-01 02:05:08 +0800

    Update event-dates.js

--- time/event-dates.js
+++ time/event-dates.js
@@ -1,4 +1,4 @@
-let event_dates_list = '{"event_dates":[{"event_name":"mcsls", "event_date":"2023-12-29", "otherdesc_begin":"", "otherdesc_after":"" }, {"event_name":"product_event", "event_date":"", "otherdesc_begin":"Unavaliable", "otherdesc_after":"" }, {"event_name":"misc_event", "event_date":"2023-06-02", "otherdesc_begin":"To be announced", "otherdesc_after":"" }]}';
+let event_dates_list = '{"event_dates":[{"event_name":"mcsls", "event_date":"", "otherdesc_begin":"To be announced", "otherdesc_after":"" }, {"event_name":"product_event", "event_date":"", "otherdesc_begin":"Unavaliable", "otherdesc_after":"" }, {"event_name":"misc_event", "event_date":"2023-06-02", "otherdesc_begin":"To be announced", "otherdesc_after":"" }]}';
 // {"event_name":"event_name", "event_date":"YYYY-MM-DD", "otherdesc_begin":"To be announced", "otherdesc_after":"" }
 // Not specifying days will not be shown, thanks to the artificial addition of auto checks.
 // To show that the event does not have the set date, mark it To be announced at the customized
